a17f518a805c4196f203ea064b2117a357a51fe1
Morgan Courbet <nagromc@users.noreply.github.com>
2015-05-10 19:27:26 +0200

    Fix indentation

--- static/client/controllers.js
+++ static/client/controllers.js
@@ -6,7 +6,7 @@ var wwwolClient = angular.module('wwwol-client', []);
  * List the {@link Host}
  */
 wwwolClient.controller('hostsList', function($scope, $http) {
-  $http.get(API_URL + 'hosts').success(function(data) {
-    $scope.hosts = data;
-  });
+    $http.get(API_URL + 'hosts').success(function(data) {
+        $scope.hosts = data;
+    });
 });
